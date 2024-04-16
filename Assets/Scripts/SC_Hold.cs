dcc7ea5c85f60f684418041c59cfd0052f902d44
tucoff <baracho.arturraffael@gmail.com>
2024-04-16 15:59:17 -0300

    Update

--- Assets/Scripts/SC_Hold.cs
+++ Assets/Scripts/SC_Hold.cs
@@ -3,22 +3,55 @@ using UnityEngine;
 public class SC_Hold : MonoBehaviour
 {
     bool holding = false;
+    public GameObject dogInHand = null;
+    public GameObject nearestDogInRange = null;
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.E) && !holding)
         {
-            transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
-            transform.GetChild(0).GetChild(3).gameObject.SetActive(false);
-            transform.GetChild(0).GetChild(6).gameObject.SetActive(true);
-            holding = true;
+                transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
+                transform.GetChild(0).GetChild(3).gameObject.SetActive(false);
+                transform.GetChild(0).GetChild(6).gameObject.SetActive(true);
+                nearestDogInRange.transform.parent = transform.GetChild(0).GetChild(6);
+                nearestDogInRange.transform.position = transform.GetChild(0).GetChild(6).position;
+                nearestDogInRange.GetComponent<Rigidbody>().isKinematic = true;
+                nearestDogInRange.GetComponent<Rigidbody>().detectCollisions = false;
+                holding = true;
+                dogInHand = nearestDogInRange.gameObject;
         }
         else if (Input.GetKeyUp(KeyCode.E))
         {
-            transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
-            transform.GetChild(0).GetChild(3).gameObject.SetActive(true);
-            transform.GetChild(0).GetChild(6).gameObject.SetActive(false);
-            holding = false;
+                transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
+                transform.GetChild(0).GetChild(3).gameObject.SetActive(true);
+                transform.GetChild(0).GetChild(6).gameObject.SetActive(false);
+                dogInHand.GetComponent<Rigidbody>().isKinematic = false;
+                dogInHand.GetComponent<Rigidbody>().detectCollisions = true;
+                dogInHand.transform.parent = GameObject.Find("Dogs").transform;
+                holding = false;
+                dogInHand = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider col) {
+        if (col.tag == "Dog")
+        {
+            float dist = Vector3.Distance(col.transform.position, transform.position);
+            if (!nearestDogInRange) 
+            {
+                nearestDogInRange = col.gameObject;
+            }
+            else if (dist < Vector3.Distance(nearestDogInRange.transform.position, transform.position))
+            {
+                nearestDogInRange = col.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider col) {
+        if (nearestDogInRange == col.gameObject)
+        {
+            nearestDogInRange = null;
         }
     }
 }
