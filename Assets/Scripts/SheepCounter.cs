ca9d0343eeef7b28bfccdfd97870ed77ee38372e
tucoff <baracho.arturraffael@gmail.com>
2024-05-04 19:45:44 -0300

    Update

--- Assets/Scripts/SheepCounter.cs
+++ Assets/Scripts/SheepCounter.cs
@@ -10,49 +10,52 @@ public class SheepCounter : MonoBehaviour
     private void FixedUpdate() {
         if(sheeps >= 5 && dogs == 1)
         {
-            dogs++;
+            dogs = 2;
             GameObject.FindWithTag("Dogs").transform.GetChild(1).localPosition = new Vector3(-4, 9.25f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(1).localRotation = Quaternion.Euler(0f,0f,0f);
         }
 
         if(sheeps >= 10 && dogs == 2)
         {
-            dogs++;
+            dogs = 3;
             GameObject.FindWithTag("Dogs").transform.GetChild(0).localPosition = new Vector3(-4, 8f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(0).GetComponent<AkiraBehaviour>().enabled = true;
         }
 
         if(sheeps >= 20 && dogs == 3)
         {
-            dogs++;
+            dogs = 4;
             GameObject.FindWithTag("Dogs").transform.GetChild(2).localPosition = new Vector3(-4, 9.25f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(2).localRotation = Quaternion.Euler(0f,0f,0f);
+            GameObject.FindWithTag("Dogs").transform.GetChild(2).GetComponent<SofiaBehaviour>().enabled = true;
         }
 
         if(sheeps >= 40 && dogs == 4)
         {
-            dogs++;
+            dogs = 5;
             GameObject.FindWithTag("Dogs").transform.GetChild(3).localPosition = new Vector3(-4, 9.25f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(3).localRotation = Quaternion.Euler(0f,0f,0f);
+            GameObject.FindWithTag("Dogs").transform.GetChild(3).GetComponent<LorenBehaviour>().enabled = true;
         }
 
         if(sheeps >= 60 && dogs == 5)
         {
-            dogs++;
+            dogs = 6;
             GameObject.FindWithTag("Dogs").transform.GetChild(4).localPosition = new Vector3(-4, 8f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(4).localRotation = Quaternion.Euler(0f,0f,0f);
+            GameObject.FindWithTag("Dogs").transform.GetChild(4).GetComponent<PretaBehaviour>().enabled = true;
         }
 
         if(sheeps >= 80 && dogs == 6)
         {
-            dogs++;
+            dogs = 7;
             GameObject.FindWithTag("Dogs").transform.GetChild(5).localPosition = new Vector3(-4, 9.25f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(5).localRotation = Quaternion.Euler(0f,0f,0f);
         }
 
         if(sheeps >= 100 && dogs == 7)
         {
-            dogs++;
+            dogs = 8;
             GameObject.FindWithTag("Dogs").transform.GetChild(6).localPosition = new Vector3(-4, 9.25f, -5f);
             GameObject.FindWithTag("Dogs").transform.GetChild(6).localRotation = Quaternion.Euler(0f,0f,0f);
         }
